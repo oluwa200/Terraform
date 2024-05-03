2c7586384850c04211e5c75efe3371d6cd36aa3d
oluwchei Daa <daagnambodenon@gmail.com>
2024-05-02 22:17:05 -0400

    added modified file

--- CloudTrail-project/CoudTrail.tf
+++ CloudTrail-project/CoudTrail.tf
@@ -2,13 +2,13 @@ resource "aws_cloudtrail" "example" {
   depends_on = [aws_s3_bucket_policy.example]
 
   name                          = "example"
-  s3_bucket_name                = aws_s3_bucket.example.id
+  s3_bucket_name                = aws_s3_bucket.tf-test-trail1992.id
   s3_key_prefix                 = "prefix"
   include_global_service_events = false
 }
 
-resource "aws_s3_bucket" "example" {
-  bucket        = "tf-test-trail"
+resource "aws_s3_bucket" "Nohemi1992" {
+  bucket        = "tf-test-trail1992"
   force_destroy = true
 }
 
